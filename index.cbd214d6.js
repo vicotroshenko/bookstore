d1e28e89032ca1a6834d81125ccd5e545589af5b
vicotroshenko <118654114+vicotroshenko@users.noreply.github.com>
2023-05-21 07:41:59 +0000

    Deploying to gh-pages from @ vicotroshenko/bookstore@e7d75bef63f7442e90094933f645becf2bfbcc50 🚀

---
+++ index.cbd214d6.js
@@ -0,0 +1,2 @@
+const o=document.getElementById("myBtn");o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0})),window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"};
+//# sourceMappingURL=index.cbd214d6.js.map
---
+++ index.e98e8b68.js
@@ -0,0 +1,2 @@
+!function(){var o=document.getElementById("myBtn");o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0})),window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"}}();
+//# sourceMappingURL=index.e98e8b68.js.map
